1cfda947dab3aca5ee0ee96da32655a5457d98cb
Kaninor <dariushroouhifard@gmail.com>
2021-10-19 02:52:46 +0330

    now we have favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,77 +1,79 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
 
-        <script src="{{ asset("js/jquery.js") }}"></script>
-        <!-- Custom fonts for this template-->
-        <link href="{{ asset("css/all.css") }}" rel="stylesheet" type="text/css">
-        <link
-            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-            rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{ asset('cms.png') }}">
+    <script src="{{ asset("js/jquery.js") }}">
+    </script>
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset("css/all.css") }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-        <!-- Custom styles for this template-->
-        <link href="{{ asset("css/new.css") }}" rel="stylesheet">
-        @yield('head')
-    </head>
-    <body id="page-top">
-        @yield('body')
+    <!-- Custom styles for this template-->
+    <link href="{{ asset("css/new.css") }}" rel="stylesheet">
+    @yield('head')
+</head>
 
-        <script src="{{ asset("js/bootstrapjs.js") }}"></script>
+<body id="page-top">
+    @yield('body')
 
-        <!-- Core plugin JavaScript-->
-        <script src="{{ asset("js/jqueryeasing.js") }}"></script>
+    <script src="{{ asset("js/bootstrapjs.js") }}"></script>
 
-        <!-- Custom scripts for all pages-->
-        <script src="{{ asset("js/sb.js") }}"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset("js/jqueryeasing.js") }}"></script>
 
-        <!-- Page level plugins -->
-        <script src="{{ asset("js/chart.js") }}"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset("js/sb.js") }}"></script>
 
-        <!-- Page level custom scripts -->
-        <script src="{{ asset("js/chartaria.js") }}"></script>
-        <script>
-            // Set new default font family and font color to mimic Bootstrap's default styling
-            Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-            Chart.defaults.global.defaultFontColor = '#858796';
+    <!-- Page level plugins -->
+    <script src="{{ asset("js/chart.js") }}"></script>
 
-            // Pie Chart Example
-            var ctx = document.getElementById("myPieChart");
-            var myPieChart = new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    labels: ["Direct", "Referral", "Social"],
-                    datasets: [{
-                        data: [<?= 10 ?>, <?= 12 ?>, <?= 16 ?>],
-                        backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-                        hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-                        hoverBorderColor: "rgba(234, 236, 244, 1)",
-                    }],
+    <!-- Page level custom scripts -->
+    <script src="{{ asset("js/chartaria.js") }}"></script>
+    <script>
+        // Set new default font family and font color to mimic Bootstrap's default styling
+        Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+        Chart.defaults.global.defaultFontColor = '#858796';
+
+        // Pie Chart Example
+        var ctx = document.getElementById("myPieChart");
+        var myPieChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ["Direct", "Referral", "Social"],
+                datasets: [{
+                    data: [<?= 10 ?>, <?= 12 ?>, <?= 16 ?>],
+                    backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+                    hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+                    hoverBorderColor: "rgba(234, 236, 244, 1)",
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
                 },
-                options: {
-                    maintainAspectRatio: false,
-                    tooltips: {
-                        backgroundColor: "rgb(255,255,255)",
-                        bodyFontColor: "#858796",
-                        borderColor: '#dddfeb',
-                        borderWidth: 1,
-                        xPadding: 15,
-                        yPadding: 15,
-                        displayColors: false,
-                        caretPadding: 10,
-                    },
-                    legend: {
-                        display: false
-                    },
-                    cutoutPercentage: 80,
+                legend: {
+                    display: false
                 },
-            });
-        </script>
-        <script src="{{ asset("js/searchbtn.js") }}"></script>
-        @yield('scripts')
-    </body>
+                cutoutPercentage: 80,
+            },
+        });
+    </script>
+    <script src="{{ asset("js/searchbtn.js") }}"></script>
+    @yield('scripts')
+</body>
+
 </html>
--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel="icon" href="{{ asset('cms.png') }}">
     <script src="{{ asset("js/jquery.js") }}"></script>
     <!-- Custom fonts for this template-->
     <link href="{{ asset("css/all.css") }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{ asset("css/new.css") }}" rel="stylesheet">
     @yield('head')
 </head>
+
 <body class="bg-gradient-primary">
-@yield('body')
+    @yield('body')
 
-<script src="{{ asset("js/bootstrapjs.js") }}"></script>
+    <script src="{{ asset("js/bootstrapjs.js") }}"></script>
 
-<!-- Core plugin JavaScript-->
-<script src="{{ asset("js/jqueryeasing.js") }}"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset("js/jqueryeasing.js") }}"></script>
 
-<!-- Custom scripts for all pages-->
-<script src="{{ asset("js/sb.js") }}"></script>
-@yield('scripts')
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset("js/sb.js") }}"></script>
+    @yield('scripts')
 </body>
+
 </html>
